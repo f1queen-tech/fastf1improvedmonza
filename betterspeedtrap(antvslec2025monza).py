5d52840916a5cbd4778453580346d5972a503f23
Annika Baberwal <stephierobertswrites@gmail.com>
2026-07-29 11:14:13 +0400

    Add files via upload

---
+++ betterspeedtrap(antvslec2025monza).py
@@ -0,0 +1,28 @@
+import fastf1
+from fastf1 import plotting, utils
+import matplotlib.pyplot as plt
+plotting.setup_mpl(mpl_timedelta_support=True)
+fastf1.Cache.enable_cache('/Users/annikababerwal/GCSE_CS/f1_cache')  # pls change cuz ur defo not me so u cant really access my fastf1 folder but its literally a pip install away so ye
+session = fastf1.get_session(2025, 'Monza', 'R') #change the race and the year but antonelli's only been around hor 2025 and 2026 so change the driver if u wanna go further back
+session.load()
+ant_lap = session.laps.pick_driver('ANT').pick_fastest()
+lec_lap = session.laps.pick_driver('LEC').pick_fastest()
+ant_tel = ant_lap.get_car_data().add_distance()
+lec_tel = lec_lap.get_car_data().add_distance()
+delta_time, ref_tel, compare_tel = utils.delta_time(ant_lap, lec_lap)
+fig, (ax1, ax2) = plt.subplots(
+    2, 1, figsize=(12, 8), sharex=True,
+    gridspec_kw={'height_ratios': [3, 1]}
+)
+ax1.plot(ant_tel['Distance'], ant_tel['Speed'], color="#00FFE1", label='ANT')
+ax1.plot(lec_tel['Distance'], lec_tel['Speed'], color='#E8002D', label='LEC')
+ax1.set_ylabel('Speed (km/h)')
+ax1.set_title('ANT vs LEC — Fastest Lap Speed Trace + Delta — 2025 Italian GP')
+ax1.legend()
+ax2.plot(ref_tel['Distance'], delta_time, color="#FF6600")
+ax2.axhline(0, color='grey', linewidth=0.8, linestyle='--')
+ax2.set_xlabel('Distance (m)')
+ax2.set_ylabel('Gap (s)\nLEC ahead ^ / ANT ahead v')
+#so this has 2 graphs the first is top speed the 2nd is delta 
+plt.tight_layout()
+plt.show()
